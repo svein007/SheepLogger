d5b0a0203853bba3fc3999caf1a9d0289d4630bb
svein <svein_001@hotmail.com>
2020-08-14 12:41:55 +0200

    Use Navigation Architecture Component.

--- app/src/main/java/com/example/osmdroidexample/MainActivity.kt
+++ app/src/main/java/com/example/osmdroidexample/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.osmdroidexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.osmdroidexample.ui.main.MainFragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
