240bf1735777dc38b3084270df93996ebf295dd1
svein <svein_001@hotmail.com>
2020-08-16 18:44:21 +0200

    Trip - manual logging of GPS Trail

--- app/src/main/java/com/example/osmdroidexample/ui/trip/TripFragment.kt
+++ app/src/main/java/com/example/osmdroidexample/ui/trip/TripFragment.kt
@@ -16,7 +16,10 @@ import org.osmdroid.tileprovider.modules.OfflineTileProvider
 import org.osmdroid.tileprovider.tilesource.FileBasedTileSource
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.tileprovider.util.SimpleRegisterReceiver
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Polyline
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import java.lang.Exception
@@ -25,6 +28,9 @@ class TripFragment : Fragment() {
 
     private lateinit var viewModel: TripViewModel
     private lateinit var binding: TripFragmentBinding
+
+    private val pinnedLocations = mutableListOf<GeoPoint>()
+    private val gpsTrail = Polyline() // GPS trail of current trip
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -93,6 +99,34 @@ class TripFragment : Fragment() {
             mapView.controller.animateTo(it)
         }
 
+        pinCurrentLocationButton.setOnClickListener {
+            pinCurrentLocation()
+        }
+
+        tripMapView.overlayManager.add(gpsTrail)
+
+    }
+
+    private fun pinCurrentLocation () {
+        MapAreaManager.getLastKnownLocation(requireContext())?.let {
+            pinnedLocations.add(it)
+
+            val pinnedLocationMarker = Marker(tripMapView)
+            pinnedLocationMarker.position = it
+            pinnedLocationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+
+            tripMapView.overlays.add(pinnedLocationMarker)
+        }
+
+        if (pinnedLocations.size > 0) {
+            gpsTrail.setPoints(pinnedLocations)
+        }
+
+        tripMapView.invalidate()
+
+
+        Log.d("#####", "Pinned Locations: ${pinnedLocations.toString()}")
+        Log.d("#####", "Overlays: ${tripMapView.overlays.toString()}")
     }
 
 }
