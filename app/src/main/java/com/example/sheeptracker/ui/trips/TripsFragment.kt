b1caaa09a8355440c99c56d9e08e2aaf26abf770
svein <svein_001@hotmail.com>
2020-11-10 16:59:47 +0100

    Feat: Filter trips by year

--- app/src/main/java/com/example/sheeptracker/ui/trips/TripsFragment.kt
+++ app/src/main/java/com/example/sheeptracker/ui/trips/TripsFragment.kt
@@ -2,19 +2,32 @@ package com.example.sheeptracker.ui.trips
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.sheeptracker.R
+import com.example.sheeptracker.database.AppDao
 import com.example.sheeptracker.database.AppDatabase
+import com.example.sheeptracker.database.entities.Trip
 import com.example.sheeptracker.databinding.TripsFragmentBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import java.util.*
 
 class TripsFragment : Fragment() {
 
     private lateinit var viewModel: TripsViewModel
     private lateinit var binding: TripsFragmentBinding
+
+    private lateinit var appDao: AppDao
+    private lateinit var adapter: TripAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +38,10 @@ class TripsFragment : Fragment() {
             inflater, R.layout.trips_fragment, container, false
         )
 
+        setHasOptionsMenu(true)
+
         val application = requireNotNull(this.activity).application
-        val appDao = AppDatabase.getInstance(application).appDatabaseDao
+        appDao = AppDatabase.getInstance(application).appDatabaseDao
 
         val viewModelFactory = TripsViewModelFactory(application, appDao)
 
@@ -37,7 +52,7 @@ class TripsFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
 
-        val adapter = TripAdapter(TripListItemListener { tripId ->
+        adapter = TripAdapter(TripListItemListener { tripId ->
             findNavController().navigate(
                 TripsFragmentDirections.actionTripsFragmentToTripDetailsFragment(tripId)
             )
@@ -49,11 +64,103 @@ class TripsFragment : Fragment() {
 
         viewModel.trips.observe(viewLifecycleOwner, {
             it?.let {
-                adapter.submitList(it)
+                updateAdapter(it, viewModel.filter.value)
             }
         })
 
+        viewModel.filter.observe(viewLifecycleOwner) {
+            it?.let {
+                updateAdapter(viewModel.trips.value, it)
+            }
+        }
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.trips_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.mi_filter_trips) {
+            showFilterPopup(requireActivity().findViewById(R.id.mi_filter_trips))
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showFilterPopup(v: View) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val popup = PopupMenu(requireContext(), v)
+
+            popup.menu.clear()
+            popup.menu.add(Menu.FIRST, Menu.FIRST, Menu.FIRST, R.string.all).isCheckable = true
+
+            val tripYears = getTripYears()
+
+            for ((i, year) in tripYears.withIndex()) {
+                popup.menu.add(Menu.FIRST, Menu.FIRST+1+i, Menu.FIRST+1+i, year).apply {
+                    isCheckable = true
+                }
+            }
+
+            popup.setOnMenuItemClickListener { item ->
+                viewModel.filter.value = Pair(item.itemId, item.title.toString())
+
+                when (item.itemId) {
+                    Menu.FIRST -> {
+                        updateTitlebar(getString(R.string.my_trips))
+                        return@setOnMenuItemClickListener true
+                    }
+                    else -> {
+                        updateTitlebar(item.title.toString())
+                        return@setOnMenuItemClickListener true
+                    }
+                }
+            }
+
+            popup.menu.setGroupCheckable(Menu.FIRST, true, true)
+
+            val checkedItemId = viewModel.filter.value?.first ?: 0
+            popup.menu.findItem(checkedItemId)?.isChecked = true
+
+            popup.show()
+        }
+    }
+
+    private suspend fun getTripYears(): List<String> {
+        return withContext(Dispatchers.IO) {
+            appDao.getFinishedTripsAsc().map { trip ->
+                val cal = Calendar.getInstance().apply{
+                    time = trip.tripDate
+                }
+                cal.get(Calendar.YEAR).toString()
+            }.distinct().sortedDescending()
+        }
+    }
+
+    private fun updateTitlebar(title: String) {
+        try {
+            (requireActivity() as AppCompatActivity).supportActionBar?.title = title
+        } catch (e: Exception) {}
+    }
+
+    private fun updateAdapter(trips: List<Trip>?, filter: Pair<Int, String>?) {
+        if (trips != null) {
+            if (filter == null) {
+                adapter.submitList(trips)
+            } else {
+                val filteredTrips = trips.filter { trip ->
+                    val cal = Calendar.getInstance().apply{
+                        time = trip.tripDate
+                    }
+                    val year = cal.get(Calendar.YEAR).toString()
+                    filter.first == Menu.FIRST || year == filter.second
+                }
+                adapter.submitList(filteredTrips)
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/sheeptracker/ui/trips/TripsViewModel.kt
+++ app/src/main/java/com/example/sheeptracker/ui/trips/TripsViewModel.kt
@@ -3,7 +3,9 @@ package com.example.sheeptracker.ui.trips
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
+import com.example.sheeptracker.R
 import com.example.sheeptracker.database.AppDao
 import com.example.sheeptracker.database.entities.Trip
 import kotlinx.coroutines.*
@@ -17,6 +19,8 @@ class TripsViewModel(
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     /** ViewModel fields **/
+
+    val filter = MutableLiveData(Pair(1, getApplication<Application>().getString(R.string.all)))
 
     val trips: LiveData<List<Trip>> = appDao.getFinishedTripsLDDesc()
 
